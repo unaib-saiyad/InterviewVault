fdf82d991b84064abb3b00998af4b9f71ecdd629
Rushwan07 <rushdeveloper07@gmail.com>
2026-04-20 15:36:50 +0530

    refactor: dashboard grouping

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
